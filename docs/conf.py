3974c8240481013a66f09d641e4c872e894be8e8
Dipson-Wang <105050041+Dipson-Wang@users.noreply.github.com>
2022-07-28 14:32:53 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ author = 'RoboMaster'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    'recommonmark',
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
